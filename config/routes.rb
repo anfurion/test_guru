df2c78a8d6db620b0b41078efbb59d292986c1d0
Anton Sinyuk <anton.sinyuk@gmail.com>
2021-08-25 00:07:56 +0300

    made about pages static

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :about, only: :index do
-    collection do
-      get '/author', to: 'about#author'
-    end
-  end
 end
