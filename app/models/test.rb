fb2f60c81a66cfbabe4c9299cbcf0dcefa0ad185
Anton Sinyuk <anton.sinyuk@gmail.com>
2021-10-30 00:19:11 +0300

    added class methods to models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,9 @@
 class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
+
+  def self.by_category(category_title)
+    category = Category.by_title(category_title)
+    where(category: category).order(id: :desc).pluck(:title)
+  end
 end
